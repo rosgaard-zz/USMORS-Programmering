f43f3bc2ece0ea2eec981ba978a8becc738c363d
Thomas Rosgaard <msn@rosgaard.net>
2024-12-04 09:27:36 +0100

    Tilføjet Dice

---
+++ Dice/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Dice
+{
+    internal class Program
+    {
+        private static int minimum = 1;
+        private static int maximum = 6;
+        private static Random random;
+        private static int throws = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n* * * * * * TERNING * * * * * *\n");
+            Console.WriteLine("Tryk på en tast for at slå en terning. Tryk på Q for at afslutte.\n");
+
+            random = new Random();
+
+            //Så længe at tasten der trykkes på ikke er Q
+            while (Console.ReadKey().Key != ConsoleKey.Q)
+            {
+                RollDice();
+            }
+        }
+
+
+        /// <summary>
+        /// Kast en terning
+        /// </summary>
+        private static void RollDice()
+        {
+            Console.Clear();
+            Console.WriteLine("\n* * * * * * TERNING * * * * * *\n");
+            Console.WriteLine("Tryk på en tast for at slå en terning. Tryk på Q for at afslutte.\n");
+
+            //Lav et tilfældigt heltal mellem minimum og maximum
+            int die = random.Next(minimum, maximum + 1);
+
+            //Læg én til antal kast
+            throws++;
+            
+            //Udskriv kast nummer og det tilfældige tal
+            Console.WriteLine($"\nKast nummer {throws}. Du slog: {die}\n");
+        }
+    }
+}
